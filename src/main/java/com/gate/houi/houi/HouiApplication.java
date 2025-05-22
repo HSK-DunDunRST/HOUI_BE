e27ed7790b573f2394b0daaa953ec081b9031baf
HSK-DunDunRST <gon5073@naver.com>
2025-05-22 16:50:00 +0900

    레포지토리 및 프로젝트 생성

---
+++ src/main/java/com/gate/houi/houi/HouiApplication.java
@@ -0,0 +1,13 @@
+package com.gate.houi.houi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gate/houi/houi/HouiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gate.houi.houi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
